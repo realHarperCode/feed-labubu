c1b37c496b96d4d43f4a34f6c04fae91c9532cc6
Harper <harperagamez@gmail.com>
2025-06-26 16:23:13 +0100

    Updated ServerScriptService/Server/Components/Data/Daily.lua

---
+++ ServerScriptService/Server/Components/Data/Daily.lua
@@ -0,0 +1,31 @@
+-- @ScriptType: ModuleScript
+return {
+	[1] = {
+		Multiplier = 1.1,
+		Type = 1
+	},
+	[2] = {
+		Multiplier = 1.1,
+		Type = 1
+	},
+	[3] = {
+		Multiplier = 1.1,
+		Type = 1
+	},
+	[4] = {
+		Multiplier = 1.1,
+		Type = 1
+	},
+	[5] = {
+		Multiplier = 1.1,
+		Type = 1
+	},
+	[6] = {
+		Multiplier = 1.1,
+		Type = 1
+	},
+	[7] = {
+		Multiplier = 1.1,
+		Type = 1
+	}
+}
