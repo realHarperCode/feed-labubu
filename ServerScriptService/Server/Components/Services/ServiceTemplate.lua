af26e69c1d1b04f515da245048327743d90004fd
Harper <harperagamez@gmail.com>
2025-06-26 16:36:21 +0100

    Updated ServerScriptService/Server/Components/Services/ServiceTemplate.lua

---
+++ ServerScriptService/Server/Components/Services/ServiceTemplate.lua
@@ -0,0 +1,52 @@
+-- @ScriptType: ModuleScript
+--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
+--> Services
+
+local TweenService = game:GetService("TweenService")
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+
+local ModuleLoader = require(ReplicatedStorage.ModuleLoader)
+local Network = require(ReplicatedStorage.Database.Security.Network)
+
+local ServiceTemplate = {}
+
+--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
+--> Modules
+
+
+
+--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
+--> Private Variables
+
+
+
+--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
+--> Private Functions
+
+
+
+--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
+--> Public Functions
+
+function ServiceTemplate:Toggle(Player: Player, temporaryDuration: number?)
+	
+end
+
+--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
+--> Initialize
+
+ModuleLoader:OnStart(function()
+	
+end)
+
+--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
+--> Events
+
+
+
+--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==--==
+--> Connections
+
+
+
+return ServiceTemplate
